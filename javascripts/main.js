79bfa18308bdd3e5c902ec65ebf82916e54ea129
Jim Vickery <jimrv8@yahoo.com>
2017-08-17 16:19:32 -0500

    added comments

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,29 +1,39 @@
 "use strict";
 console.log("main.js active");
+//******************** */
+//standard handlebars tool reuired for templates
 let Handlebars = require('hbsfy/runtime');
+
+//******************** */
+//varialbe thats pull in attraction factory
 let parkAreas = require('./attraction-factory.js');
+
+//******************** */
+//this handlebars template each loop to get park grid data
 let gridTemplate = require('../templates/parkGrid.hbs');
 
-
+//******************** */
+//this increments the handlebars index by one so it starts 1
 Handlebars.registerHelper("incrementer", (value) => parseInt(value) + 1);
-//populate data
-// function populateGrid(parkNames) {
-// let newDiv = document.createElement("div");
-// newDiv.innerHTML = funAreas(parkNames);
-// $("#grid1").append(newDiv);
-// }
-// funAreas.getAreas = () => {
-// } 
 
+//******************** */
+//a getAreas method that uses a promise to get the data from attraction factory
 parkAreas.getAreas()
 .then((data) => {
 	console.log('from main.js data', data);
+
+	//******************** */
+	//calls the function inside this promise so that it executes when its needed
 	gridData(data);
 },
+//******************** */
+//reject message in promise to send if ajax callback fails
 (reject) => {
 	console.log('somthing went wrong in main.js');
 });
 
+//******************** */
+//function that takes the object data and appends it in the DOM at the grid id
 function gridData(data){
 	let newDiv = document.createElement("div");
 	newDiv.innerHTML = gridTemplate(data);
